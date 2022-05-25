7585bb6cd7c329dcf031b1371886f80793fc1ab3
Alex Fix <oleksandr.papish02052001@gmail.com>
2022-05-25 23:56:03 +0300

    Improved discipline table. Added expandable row

--- Source/LoadDistribution/LoadDistribution.WebAPI/ClientApp/src/app/components/discipline/discipline-list/discipline-list.component.ts
+++ Source/LoadDistribution/LoadDistribution.WebAPI/ClientApp/src/app/components/discipline/discipline-list/discipline-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import DisciplineClient from 'src/app/clients/disciplineClient.client';
 import { ProjectHandler } from 'src/app/helpers/projectHandler.helper';
@@ -7,13 +8,26 @@ import DisciplineDTO from 'src/app/models/dto/models/disciplineDTO.model';
 @Component({
   selector: 'app-discipline-list',
   templateUrl: './discipline-list.component.html',
-  styleUrls: ['./discipline-list.component.scss']
+  styleUrls: ['./discipline-list.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class DisciplineListComponent extends TableComponent<DisciplineDTO> {
+  expandedDiscipline: DisciplineDTO | null;
+
   constructor(
     disciplineClient: DisciplineClient,
     projectHanlder: ProjectHandler
   ) { 
-    super(disciplineClient, ['term', 'educationLevel', 'educationForm', 'planIndex', 'speciality', 'groupAbbreviation', 'specialization', 'institute', 'course', 'studentCount', 'budgetStudentCount', 'comercialStudentCount', 'groupCount', 'subgroupCount', 'threadCount', 'name', 'type', 'universityId', 'universityName'], projectHanlder);
+    super(disciplineClient, ['term', 'educationForm', 'groupAbbreviation', 'institute', 'course', 'name', 'type', 'universityName'], projectHanlder);
+  }
+
+  onDisciplineRowClick(discipline: DisciplineDTO) {
+    this.expandedDiscipline = this.expandedDiscipline == discipline ? null : discipline;
   }
 }
